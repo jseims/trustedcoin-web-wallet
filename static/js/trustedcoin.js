8e9915d85e249b5a4427dc0e7fa534b7fa2d1eb4
Joshua Seims <jseims@hotmail.com>
2013-11-27 02:18:08 +0000

    changed from Satoshis to BTC, and started "save password encrypted version of mnemonic locally"

--- static/js/trustedcoin.js
+++ static/js/trustedcoin.js
@@ -1,280 +1,99 @@
-var myModule = angular.module('myModule', [])
-  .config(function($routeProvider, $locationProvider, $httpProvider) {
-    $routeProvider.
-      when('/', {controller:VisitorCtrl, templateUrl:'static/partials/wallet.html'}).
-      when('/create-wallet', {controller:CreateWalletCtrl, templateUrl:'static/partials/create-wallet.html'}).
-      when('/wallet/:address', {controller:WalletCtrl, templateUrl:'static/partials/view-wallet.html'}).
-      when('/gen-key', {controller:GenKeyCtrl, templateUrl:'static/partials/gen-key.html'}).
-      otherwise({redirectTo:'/'});
-  })
-    .directive('qrcode', ['$timeout', '$window', function($timeout, $window){
+var trustedcoin = trustedcoin || {};
 
-    var canvas2D = !!$window.CanvasRenderingContext2D,
-        levels = {
-          'L': 'Low',
-          'M': 'Medium',
-          'Q': 'Quartile',
-          'H': 'High'
-        },
-        draw = function(context, qr, modules, tile){
-          for (var row = 0; row < modules; row++) {
-            for (var col = 0; col < modules; col++) {
-              var w = (Math.ceil((col + 1) * tile) - Math.floor(col * tile)),
-                  h = (Math.ceil((row + 1) * tile) - Math.floor(row * tile));
-              context.fillStyle = qr.isDark(row, col) ? '#000' : '#fff';
-              context.fillRect(Math.round(col * tile), Math.round(row * tile), w, h);
-            }
-          }
-        };
-
-    return {
-      restrict: 'E',
-      template: '<canvas></canvas>',
-      link: function(scope, element, attrs){
-
-        var domElement = element[0],
-            canvas = element.find('canvas')[0],
-            version = Math.max(1, Math.min(parseInt(attrs.version, 10), 10)) || 4,
-            correction = attrs.errorCorrectionLevel in levels ? attrs.errorCorrectionLevel : 'M',
-            trim = /^\s+|\s+$/g,
-            qr = qrcode(version, correction);
-
-        qr.make();
-
-        var modules = qr.getModuleCount(),
-            size = parseInt(attrs.size, 10) || modules * 2,
-            tile = size / modules,
-            render = function(qr, text){
-              qr.addData(text);
-              qr.make();
-              if (canvas2D) {
-                draw(context, qr, modules, tile);
-              } else {
-                domElement.innerHTML = qr.createImgTag(tile, 0);
-              }
-            };
-
-        if (canvas2D) {
-          var context = canvas.getContext('2d');
-          canvas.width = canvas.height = size;
-        }
-
-        attrs.$observe('data', function(value){
-          if (!value) {
-            return;
-          }
-          var text = value.replace(trim, ''),
-              qr = qrcode(version, correction);
-          render(qr, text);
-        });
-      }
-    };
-  }])
-  .filter('truncate', function () {
-        return function (text, length, end) {
-            if (isNaN(length))
-                length = 10;
- 
-            if (end === undefined)
-                end = "...";
- 
-            if (text.length <= length || text.length - end.length <= length) {
-                return text;
-            }
-            else {
-                return String(text).substring(0, length-end.length) + end;
-            }
- 
-        };
-    })
-  .run(function($rootScope, $log, $location, $filter, $http, $timeout, $window) {
-    $rootScope.site_name = 'Trusted Coin';
-	$rootScope.wallet = { };
-});
-
-function VisitorCtrl($scope, $rootScope, $http, $location, $routeParams, $log) {
-}
-
-var keyProgressCallback = function(percentDone) {
-	if(percentDone % 5 == 0) {
-		$('#seed-progress').css('width', (percentDone + 19) + '%'); 
-	}
+trustedcoin.generate_mnemonic = function() {
+    var pk = new Array(32);
+    rng_get_bytes(pk);
+    var seed = Crypto.util.bytesToHex(pk.slice(0,16));
+    // nb! electrum doesn't handle trailing zeros very well
+    // and we want to stay compatible.
+    if (seed.charAt(0) == '0') seed = seed.substr(1);
+    return mn_encode(seed);
 };
 
-function GenKeyCtrl($scope, $rootScope, $http, $location, $routeParams, $log) {
+trustedcoin.mnemoic_to_key = function(mnemonic, keyReadyCallback, keyProgressCallback) {
+    var seed = mn_decode(mnemonic);
+    
+    Electrum.init(seed, keyProgressCallback, function(key) {
+        Electrum.gen(1, function(r) {
+            var key = new Bitcoin.ECKey(r[1]);
+            key.setCompressed(true);
+            keyReadyCallback({
+                key: key,
+                address: key.getBitcoinAddress().toString(),
+                privateKey: key.getExportedPrivateKey(),
+                publicKey: Crypto.util.bytesToHex(key.getPub())
+            });
+        });
+    });
+};
 
-    $scope.mnemonic = tc_crypto.generate_mnemonic();
-	$scope.key = null;
-	$scope.show_progress = false;
-	
-	var keyReadyCallback = function(key) {
-		//$log.log(key);
-		$scope.show_progress = false;
-		
-		$scope.$apply(function() {
-			$scope.key = key;
-		});
-	}
-	
-	$scope.refresh_mnemonic = function() {
-		$scope.mnemonic = tc_crypto.generate_mnemonic();
-		$scope.key = null;
-		$('#seed-progress').css('width', '0%');
-		$scope.show_progress = false;
-	}
-	
-	$scope.generate_key = function() {
-		$scope.show_progress = true;
-        tc_crypto.mnemoic_to_key($scope.mnemonic, keyReadyCallback, keyProgressCallback);	
-	}
-}
+trustedcoin.encrypt = function(clear_text, key) {
+	return clear_text;
+};
 
-function CreateWalletCtrl($scope, $rootScope, $http, $location, $routeParams, $log, $timeout) {
-	$scope.state = "choose_backup";
-    $scope.backup_mnemonic = tc_crypto.generate_mnemonic();
-    $scope.primary_mnemonic = tc_crypto.generate_mnemonic();
-	$scope.show_progress = false;
+trustedcoin.decrypt = function(cipher_text, key) {
+	return cipher_text;
+};
 
-	$scope.refresh_backup_mnemonic = function() {
-		$scope.backup_mnemonic = tc_crypto.generate_mnemonic();
-	};
+trustedcoin.sign_transaction = function(private_key, raw_transaction) {
+	return "<signed transaction>";
+};
 
-	$scope.refresh_primary_mnemonic = function() {
-		$scope.primary_mnemonic = tc_crypto.generate_mnemonic();
-	};
-	
-	var backupKeyReadyCallback = function(key) {
-		//$log.log(key);
-		$scope.show_progress = false;
-		
-		$scope.$apply(function() {
-			$scope.secondary_public_key = key.publicKey;
-		});
-		$scope.state = "choose_primary";
-		$scope.$apply();
-	};
-	
-	$scope.generate_backup_key = function() {
-		$scope.show_progress = true;
-        tc_crypto.mnemoic_to_key($scope.backup_mnemonic, backupKeyReadyCallback, keyProgressCallback);	
-	};
-	
-	var primaryKeyReadyCallback = function(key) {
-		//$log.log(key);
-		$scope.show_progress = false;
-		
-		$scope.$apply(function() {
-			$scope.primary_public_key = key.publicKey;
-		});
-		$scope.state = "verify_primary";
-		$scope.$apply();
-	};
-	
-	$scope.generate_primary_key = function() {
-		$scope.show_progress = true;
-        tc_crypto.mnemoic_to_key($scope.primary_mnemonic, primaryKeyReadyCallback, keyProgressCallback);	
-	};
-	
-	$scope.verify_primary_mnemonic = function() {
-		if ($scope.verify_mnemonic == $scope.primary_mnemonic) {
-			$timeout(function() {
-				$scope.state = "enter_contacts";
-				$scope.$apply();
-			});
-		} else {
-			$scope.not_verified = true;
-		}
-	}
-	
-	var createWalletCallback = function(data) {
-		$log.log(data);
-		$location.path("/wallet/" + data.address);
-		$scope.$apply();		
-	};
-	
-	var errorCallback = function(text) {
-		$scope.error = text;
-		$scope.$apply();
-		$log.log(text);
-	};		
-	
-	
-	$scope.create_wallet = function() {
-		$log.log("primary public key: " + $scope.primary_public_key);
-		$log.log("secondary public key: " + $scope.secondary_public_key);
-		$log.log("phone " + $scope.phone);
-		$log.log("email " + $scope.email);
-		if (!$scope.primary_public_key || !$scope.secondary_public_key) {
-			$scope.error = "You're missing a private key -- try starting over";
-		} else if (!$scope.email && !$scope.phone) {
-			$scope.error = "You need to provide at least one contact channel";
-		} else {
-			policy = {"type" : "latency", "delay_in_seconds": 86400, "contacts":  [{"email" : $scope.email}, {"sms" : $scope.phone}]};
-			tc_crypto.create_cosigner($scope.primary_public_key, $scope.secondary_public_key, policy, createWalletCallback, errorCallback);
-		}
-	}	
-	
-}	
+trustedcoin.create_cosigner = function(primary_key, secondary_key, policy, success_callback, error_callback) {
+	jQuery.ajax({
+			  url: "https://api.trustedcoin.com/1/cosigner",
+			  type: "POST",
+			  data: JSON.stringify({"primary_key": primary_key, "secondary_key": secondary_key, "policy" : policy}),
+			  dataType: "json",
+			  contentType : 'application/json',		  
+			  beforeSend: function(x) {
+				if (x && x.overrideMimeType) {
+				  x.overrideMimeType("application/j-son;charset=UTF-8");
+				}
+			  },
+			  success: success_callback,
+			  error: function(data) {
+				var errorObj = JSON.parse(data.responseText);
+				error_callback(errorObj.message);
+			  }
+	});
+};
 
+trustedcoin.get_cosigner = function(address, success_callback, error_callback) {
+	jQuery.ajax({
+			  url: "https://api.trustedcoin.com/1/cosigner/" + address,
+			  type: "GET",
+			  dataType: "json",
+			  contentType : 'application/json',		  
+			  beforeSend: function(x) {
+				if (x && x.overrideMimeType) {
+				  x.overrideMimeType("application/j-son;charset=UTF-8");
+				}
+			  },
+			  success: success_callback,
+			  error: function(data) {
+				var errorObj = JSON.parse(data.responseText);
+				error_callback(errorObj.message);
+			  }
+	});
+};
 
-function WalletCtrl($scope, $rootScope, $http, $location, $routeParams, $log) {
-	$scope.address = $routeParams.address;
-	
-	var balanceCallback = function(text) {
-		$scope.balance = text;		
-		$scope.$apply();		
-	};
-	
-	
-	BLOCKCHAIN.retrieveBalance($scope.address, balanceCallback);
-	
-	var getWalletCallback = function(data) {
-		$scope.wallet = data;		
-		$scope.$apply();		
-	};
-	
-	var getErrorCallback = function(text) {
-		$scope.get_error= text;
-		$scope.$apply();
-	};	
-	
-	tc_crypto.get_cosigner($scope.address, getWalletCallback, getErrorCallback);
-	
-	var startSendCallback = function(data) {
-		$log.log(data);
-		$scope.unsigned_transaction = data.unsigned_transaction;
-		$scope.prompt_mnemonic = true;
-		$scope.send_error = null;
-		$scope.$apply();
-	};	
-	
-	var sendErrorCallback = function(text) {
-		$scope.send_error = text;
-		$scope.$apply();
-	};	
-	
-	
-	$scope.send_btc = function() {
-		var amount = parseInt($scope.to_amount);
-		$log.log("sending " + amount + " to " + $scope.to_address);
-		tc_crypto.start_send($scope.address, $scope.to_address, amount, startSendCallback, sendErrorCallback);
-	};
-	
-	var keyReadyCallback = function(key) {
-		//$log.log(key);
-		$scope.show_progress = false;
-		
-		$scope.$apply(function() {
-			$scope.key = key;
-		});
-		$log.log("signing " + $scope.unsigned_transaction + " with " + $scope.key.privateKey);
-	};
-	
-	$scope.sign_transaction = function() {
-		$scope.show_progress = true;
-        tc_crypto.mnemoic_to_key($scope.mnemonic, keyReadyCallback, keyProgressCallback);	
-	};
-	
-}
-
-
+trustedcoin.start_send = function(from_address, to_address, amount, success_callback, error_callback) {
+	jQuery.ajax({
+			  url: "https://api.trustedcoin.com/1/cosigner/" + from_address + "/send_start",
+			  type: "POST",
+			  data: JSON.stringify({"output_address": to_address, "amount": amount}),
+			  dataType: "json",
+			  contentType : 'application/json',		  
+			  beforeSend: function(x) {
+				if (x && x.overrideMimeType) {
+				  x.overrideMimeType("application/j-son;charset=UTF-8");
+				}
+			  },
+			  success: success_callback,
+			  error: function(data) {
+				var errorObj = JSON.parse(data.responseText);
+				error_callback(errorObj.message);
+			  }
+	});
+};
--- static/js/wallet.js
+++ static/js/wallet.js
@@ -1,70 +1,290 @@
-var myModule = angular.module('wallet', []);
+/*
+ * wallet.js
+ * (c) 2013 TrustedCoin http://api.trustedcoin.com
+ * License: MIT
+ */
 
-function WalletCtrl($scope, $rootScope, $http, $location, $log) {
-    
-    $log.log(Crypto.util.bytesToHex(Bitcoin.Address.decodeString('1JwSSubhmg6iPtRjtyqhUYYH7bZg3Lfy1T')));
+var myModule = angular.module('myModule', [])
+  .config(function($routeProvider, $locationProvider, $httpProvider) {
+    $routeProvider.
+      when('/', {controller:VisitorCtrl, templateUrl:'static/partials/wallet.html'}).
+      when('/create-wallet', {controller:CreateWalletCtrl, templateUrl:'static/partials/create-wallet.html'}).
+      when('/wallet/:address', {controller:WalletCtrl, templateUrl:'static/partials/view-wallet.html'}).
+      when('/gen-key', {controller:GenKeyCtrl, templateUrl:'static/partials/gen-key.html'}).
+      otherwise({redirectTo:'/'});
+  })
+  // using qrcode directive from https://github.com/monospaced/angular-qrcode
+    .directive('qrcode', ['$timeout', '$window', function($timeout, $window){
 
-    $scope.mnemonic = "steal night raise foot struggle royal laid worse embrace mutter lady chance";
-    
-    $scope.computeSeed = function(percent) {
-        
-        $log.log("computeSeed");
-        
-        var keyProgressCallback = function(percentDone) {
-            if(percentDone % 5 == 0) {
-                $('#seed-progress').css('width', (percentDone + 19) + '%'); 
+    var canvas2D = !!$window.CanvasRenderingContext2D,
+        levels = {
+          'L': 'Low',
+          'M': 'Medium',
+          'Q': 'Quartile',
+          'H': 'High'
+        },
+        draw = function(context, qr, modules, tile){
+          for (var row = 0; row < modules; row++) {
+            for (var col = 0; col < modules; col++) {
+              var w = (Math.ceil((col + 1) * tile) - Math.floor(col * tile)),
+                  h = (Math.ceil((row + 1) * tile) - Math.floor(row * tile));
+              context.fillStyle = qr.isDark(row, col) ? '#000' : '#fff';
+              context.fillRect(Math.round(col * tile), Math.round(row * tile), w, h);
             }
+          }
         };
-        
-        var keyReadyCallback = function(key) {
-            $log.log("key ready");
-            $log.log(key);
-            
-            $scope.$apply(function() {
-                $scope.keys = [key];
-            });
+
+    return {
+      restrict: 'E',
+      template: '<canvas></canvas>',
+      link: function(scope, element, attrs){
+
+        var domElement = element[0],
+            canvas = element.find('canvas')[0],
+            version = Math.max(1, Math.min(parseInt(attrs.version, 10), 10)) || 4,
+            correction = attrs.errorCorrectionLevel in levels ? attrs.errorCorrectionLevel : 'M',
+            trim = /^\s+|\s+$/g,
+            qr = qrcode(version, correction);
+
+        qr.make();
+
+        var modules = qr.getModuleCount(),
+            size = parseInt(attrs.size, 10) || modules * 2,
+            tile = size / modules,
+            render = function(qr, text){
+              qr.addData(text);
+              qr.make();
+              if (canvas2D) {
+                draw(context, qr, modules, tile);
+              } else {
+                domElement.innerHTML = qr.createImgTag(tile, 0);
+              }
+            };
+
+        if (canvas2D) {
+          var context = canvas.getContext('2d');
+          canvas.width = canvas.height = size;
         }
-        
-        tc_crypto.mnemoic_to_key($scope.mnemonic, keyReadyCallback, keyProgressCallback);
+
+        attrs.$observe('data', function(value){
+          if (!value) {
+            return;
+          }
+          var text = value.replace(trim, ''),
+              qr = qrcode(version, correction);
+          render(qr, text);
+        });
+      }
     };
-    
-    
-    $scope.generateSeed = function() {
-        $scope.mnemonic = tc_crypto.generate_mnemonic();
-    }
-    
-    $scope.signTx = function() {
-        
-        $log.log($scope.txToSign);
-        
-        $log.log($scope.keys[0]);
-        
-        var m = $scope.txToSign;
-        m = "x-timestamp: " + (new Date()).valueOf();
-        
-        $log.log("signing:" + m)
-        
-        sig = sign_message($scope.keys[0].key, m, $scope.keys[0].key.isCompressed());
-        
-        $log.log(sig);
-        
-        $log.log(verify_message(sig, m));
-        
-        var auth = 'Signature keyId="' + $scope.keys[0].address + '",algorithm="bitcoin",headers="x-timestamp",signature="' + sig + '"';
-        // we can't set the date like http://tools.ietf.org/html/draft-cavage-http-signatures-00
-        // because it's illegal to set that header in an xhr ... so we do this instead
-        var headers = {
-            'Authorization': auth,
-            'x-timestamp': m
-        };
-        $http({method: 'GET', url: '/1/test', headers: headers}).
-            success(function(data, status, headers, config) {
-                // this callback will be called asynchronously
-                // when the response is available
-            }).
-            error(function(data, status, headers, config) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-            });
-    }
+  }])
+  .run(function($rootScope, $log, $location, $filter, $http, $timeout, $window) {
+    $rootScope.site_name = 'Trusted Coin';
+});
+
+function VisitorCtrl($scope, $rootScope, $http, $location, $routeParams, $log) {
 }
+
+var keyProgressCallback = function(percentDone) {
+	if(percentDone % 5 == 0) {
+		$('#seed-progress').css('width', (percentDone + 19) + '%'); 
+	}
+};
+
+function GenKeyCtrl($scope, $rootScope, $http, $location, $routeParams, $log) {
+
+    $scope.mnemonic = trustedcoin.generate_mnemonic();
+	$scope.key = null;
+	$scope.show_progress = false;
+	
+	var keyReadyCallback = function(key) {
+		//$log.log(key);
+		$scope.show_progress = false;
+		
+		$scope.$apply(function() {
+			$scope.key = key;
+		});
+	}
+	
+	$scope.refresh_mnemonic = function() {
+		$scope.mnemonic = trustedcoin.generate_mnemonic();
+		$scope.key = null;
+		$('#seed-progress').css('width', '0%');
+		$scope.show_progress = false;
+	}
+	
+	$scope.generate_key = function() {
+		$scope.show_progress = true;
+        trustedcoin.mnemoic_to_key($scope.mnemonic, keyReadyCallback, keyProgressCallback);	
+	}
+}
+
+function CreateWalletCtrl($scope, $rootScope, $http, $location, $routeParams, $log, $timeout) {
+	$scope.state = "choose_backup";
+    $scope.backup_mnemonic = trustedcoin.generate_mnemonic();
+    $scope.primary_mnemonic = trustedcoin.generate_mnemonic();
+	$scope.show_progress = false;
+
+	$scope.refresh_backup_mnemonic = function() {
+		$scope.backup_mnemonic = trustedcoin.generate_mnemonic();
+	};
+
+	$scope.refresh_primary_mnemonic = function() {
+		$scope.primary_mnemonic = trustedcoin.generate_mnemonic();
+	};
+	
+	var backupKeyReadyCallback = function(key) {
+		//$log.log(key);
+		$scope.show_progress = false;
+		
+		$scope.$apply(function() {
+			$scope.secondary_public_key = key.publicKey;
+		});
+		$scope.state = "choose_primary";
+		$scope.$apply();
+	};
+	
+	$scope.generate_backup_key = function() {
+		$scope.show_progress = true;
+        trustedcoin.mnemoic_to_key($scope.backup_mnemonic, backupKeyReadyCallback, keyProgressCallback);	
+	};
+	
+	var primaryKeyReadyCallback = function(key) {
+		//$log.log(key);
+		$scope.show_progress = false;
+		
+		$scope.$apply(function() {
+			$scope.primary_public_key = key.publicKey;
+		});
+		$scope.state = "verify_primary";
+		$scope.$apply();
+	};
+	
+	$scope.generate_primary_key = function() {
+		$scope.show_progress = true;
+        trustedcoin.mnemoic_to_key($scope.primary_mnemonic, primaryKeyReadyCallback, keyProgressCallback);	
+	};
+	
+	$scope.verify_primary_mnemonic = function() {
+		if ($scope.verify_mnemonic == $scope.primary_mnemonic) {
+			$timeout(function() {
+				$scope.state = "enter_contacts";
+				$scope.$apply();
+			});
+		} else {
+			$scope.not_verified = true;
+		}
+	}
+	
+	var createWalletCallback = function(data) {
+		$log.log(data);
+		$location.path("/wallet/" + data.address);
+		$scope.$apply();		
+	};
+	
+	var errorCallback = function(text) {
+		$scope.error = text;
+		$scope.$apply();
+		$log.log(text);
+	};		
+	
+	
+	$scope.create_wallet = function() {
+		$log.log("primary public key: " + $scope.primary_public_key);
+		$log.log("secondary public key: " + $scope.secondary_public_key);
+		$log.log("phone " + $scope.phone);
+		$log.log("email " + $scope.email);
+		if (!$scope.primary_public_key || !$scope.secondary_public_key) {
+			$scope.error = "You're missing a private key -- try starting over";
+		} else if (!$scope.email && !$scope.phone) {
+			$scope.error = "You need to provide at least one contact channel";
+		} else {
+			policy = {"type" : "latency", "delay_in_seconds": 86400, "contacts":  [{"email" : $scope.email}, {"sms" : $scope.phone}]};
+			trustedcoin.create_cosigner($scope.primary_public_key, $scope.secondary_public_key, policy, createWalletCallback, errorCallback);
+		}
+	}	
+	
+}	
+
+
+function WalletCtrl($scope, $rootScope, $http, $location, $routeParams, $log) {
+	$scope.address = $routeParams.address;
+	var key = $scope.address + "-mnemonic";
+	$scope.encrypted_mnemonic =	localStorage.getItem(key);
+	
+	var balanceCallback = function(text) {
+		$scope.balance = Bitcoin.Util.formatValue(text);
+		$scope.$apply();		
+	};
+	
+	
+	BLOCKCHAIN.retrieveBalance($scope.address, balanceCallback);
+	
+	var getWalletCallback = function(data) {
+		$scope.wallet = data;		
+		$scope.$apply();		
+	};
+	
+	var getErrorCallback = function(text) {
+		$scope.get_error = text;
+		$scope.$apply();
+	};	
+	
+	trustedcoin.get_cosigner($scope.address, getWalletCallback, getErrorCallback);
+	
+	var startSendCallback = function(data) {
+		$log.log(data);
+		$scope.unsigned_transaction = data.unsigned_transaction;
+		$scope.prompt_sign = true;
+		$scope.send_error = null;
+		$scope.$apply();
+	};	
+	
+	var sendErrorCallback = function(text) {
+		$scope.send_error = text;
+		$scope.$apply();
+	};	
+	
+	
+	$scope.send_btc = function() {
+		var amount = parseInt($scope.to_amount);
+		$log.log("sending " + amount + " to " + $scope.to_address);
+		var satoshis = parseFloat($scope.to_amount) * 100000000;
+		if (satoshis > 0) {
+			$scope.send_error = null;
+			trustedcoin.start_send($scope.address, $scope.to_address, satoshis, startSendCallback, sendErrorCallback);
+		} else {
+			$scope.send_error = "Must enter a valid number of BTC for 'Amount'";
+			
+		}
+	};
+	
+	var keyReadyCallback = function(key) {
+		//$log.log(key);
+		$scope.show_progress = false;
+		
+		$scope.$apply(function() {
+			$scope.key = key;
+		});
+		$log.log("signing " + $scope.unsigned_transaction + " with " + $scope.key.privateKey);
+	};
+	
+	$scope.sign_transaction = function() {
+		$scope.show_progress = true;
+        trustedcoin.mnemoic_to_key($scope.mnemonic, keyReadyCallback, keyProgressCallback);	
+	};
+	
+	$scope.encrypt_mnemonic = function() {
+		$scope.encrypted_mnemonic = trustedcoin.encrypt($scope.mnemonic, $scope.password);
+		$scope.password = null;
+		var key = $scope.address + "-mnemonic";
+		localStorage.setItem(key, $scope.encrypted_mnemonic);
+	};
+	
+	$scope.remove_local_mnemonic = function() {
+		var key = $scope.address + "-mnemonic";
+		localStorage.removeItem(key);
+		$scope.encrypted_mnemonic = null;
+	};
+}
+
+
